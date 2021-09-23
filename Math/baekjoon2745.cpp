4c2838cc774ad5534ef479cfbfbbaad74a31ffda
unknown <tmdgml113@ewhain.net>
2021-09-23 22:50:51 +0900

    math 3문제

---
+++ Math/baekjoon2745.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	string N;
+	int B;
+	cin >> N>> B;
+
+	int ans = 0;
+	for (int i=0; i < N.length(); i++)
+	{
+		if (N[i] >= 'A' && N[i] <= 'Z')
+		{
+			ans = ans + (N[i] - 'A' +10) * pow(B, N.length()-1 - i);
+		}
+		else
+		{
+			ans = ans + (N[i] - '0') * pow(B, N.length()-1 - i);
+		}
+	}
+	cout << ans;
+}
---
+++ Math/baekjoon5618.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	if (n == 2)
+	{
+		int n1, n2;
+		cin >> n1 >> n2;
+
+		int minVal = min(n1, n2);
+		for (int i = 1; i <= minVal; i++)
+		{
+			if (n1 % i == 0 && n2 % i == 0)
+			{
+				cout << i << endl;
+			}
+		}
+	}
+
+	if (n == 3)
+	{
+		int n1, n2,n3;
+		cin >> n1 >> n2>>n3;
+
+		int minVal = min(min(n1, n2), n3);
+		for (int i = 1; i <= minVal; i++)
+		{
+			if (n1 % i == 0 && n2 % i == 0 && n3 % i == 0)
+			{
+				cout << i << endl;
+			}
+		}
+	}
+}
