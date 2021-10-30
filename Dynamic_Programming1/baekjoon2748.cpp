b8cdde84386a94e2004d086c999175522de696ff
unknown <tmdgml113@ewhain.net>
2021-10-30 18:39:54 +0900

    Dynamic Programming 4문제

---
+++ Dynamic_Programming1/baekjoon2748.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+long long dp[90];
+
+long long Fibo(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (dp[n] != 0)
+    {
+        return dp[n];
+    }
+    dp[n] = Fibo(n - 1) + Fibo(n - 2);
+    return dp[n];
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << Fibo(n);
+}
